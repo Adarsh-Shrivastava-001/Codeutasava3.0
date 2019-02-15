3928dd04bc750a96208ab8701e1e8afc18f590f8
Adarsh <001adarshshrivastava@gmail.com>
2019-02-15 14:43:55 +0530

    Recommeder Working

--- pycoders/home/views.py
+++ pycoders/home/views.py
@@ -10,16 +10,21 @@ import pandas as pd
 # Create your views here.
 
 def update_data():
-	mat=np.zeros((20,40))
+	user_pro_mat=np.zeros((20,40))
 	customer_set=Customer.objects.all()
 	for i in range(len(customer_set)):
 		products=Product.objects.filter(customers=customer_set[i])
-		prod_ids=[prod.id-1 for prod in products]
+		wishlist=Product.objects.filter(customers_wishlist=customer_set[i])
+		prod_ids=[prod.id-1 for prod in products]+[prod.id-1 for prod in wishlist]
 		cust_id=customer_set[i].pk
 		for j in prod_ids:
-			mat[cust_id-1,j-1]=1
-	mat=pd.DataFrame(mat)
-	mat.to_pickle('User_Prod_Label.pkl')
+			user_pro_mat[cust_id-1][j-1]=1
+
+	for i in range(len(customer)):
+		pass
+
+	user_pro_mat=pd.DataFrame(mat)
+	user_pro_mat.to_pickle('User_Prod_Label.pkl')
 
 
 
--- pycoders/recommend.py
+++ pycoders/recommend.py
@@ -11,21 +11,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-user_data=pd.read_csv('user.csv')
-prod_data=pd.read_csv('prod.csv')
 
-labels=pd.read_csv('labels.csv')
-
-user_mat=np.array(user_data)     
-prod_mat=np.array(prod_data)
 
 user_mat=np.random.random((20,10))
-prod_mat=np.random.random((20,10))
+prod_mat=np.random.random((40,10))
 
-labels=np.random.choice(2, 400, p=[0.7,0.3]).reshape(20,20)
+labels=pd.read_pickle("User_Prod_Label.pkl")
+labels=np.array(labels)
 
-epochs=1000
-eta=0.05
+epochs=500
+eta=0.005
 lam=0.01
 
 def sigmoid(arr):
@@ -41,7 +36,7 @@ for i in range(epochs):
     error=y-labels
     
     user_mat = user_mat - (error*inv_sigmoid(z)).dot(prod_mat)*eta
-    prod_mat = prod_mat - ((error*inv_sigmoid(z).T).dot(user_mat)*eta)
+    prod_mat = prod_mat - ((error*inv_sigmoid(z)).T).dot(user_mat)*eta
     
     if i%5==0:
         print("Updating Database....")
@@ -49,9 +44,7 @@ for i in range(epochs):
     
 
 
-y=np.round(y)
+np.sum(np.round(y)==labels)/800
 
-np.sum(y==labels)/400
 
-np.sum(y)
-np.sum(labels)
+q=np.round(y)
