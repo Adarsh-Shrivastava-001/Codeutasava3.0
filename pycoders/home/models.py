6b0859ddf91ba09d1a72878887151155f68ef598
ethicalrushi <pupalerusikesh@gmail.com>
2019-02-16 14:22:34 +0530

    model local changes

--- pycoders/home/models.py
+++ pycoders/home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,6 +7,7 @@ from django.db import models
 class Customer(models.Model):
 	first_name=models.CharField(max_length=25,blank=True)
 	last_name=models.CharField(max_length=25,blank=True)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	email=models.EmailField(max_length=70,blank=True)
 	contact_no=models.IntegerField()
 	address=models.CharField(max_length=100, blank=True)
@@ -19,15 +20,16 @@ class Customer(models.Model):
 	def __str__(self):
 		return self.first_name + " " + self.last_name
 
-class SearchTerm(models.Model):
-	name = models.CharField(max_length=100,blank=True, null=True)
+# class SearchTerm(models.Model):
+# 	name = models.CharField(max_length=100,blank=True, null=True)
 
-	def __str__(self):
-		return self.name
+# 	def __str__(self):
+# 		return self.name
 
 class Product(models.Model):
 	name=models.CharField(max_length=100,blank=True)
 	brand=models.ForeignKey('Brand', on_delete=models.CASCADE, related_name='products', blank=True)
+	image = models.ImageField(upload_to='')
 	category=models.ManyToManyField('Category', related_name='products', blank=True)
 	description=models.TextField(blank=False, null=False)
 	price=models.IntegerField()
