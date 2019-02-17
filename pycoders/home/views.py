38134ec107ffe032a4277a5f672649cf2fd54c7c
utkarshchoubey <utkarshchoubey629@gmail.com>
2019-02-17 09:59:30 +0530

    local

--- pycoders/home/views.py
+++ pycoders/home/views.py
@@ -39,7 +39,7 @@ def home(request):
 	print(product_list)
 
 
-	
+
 	return render(request,'home/shop_home.html', {'product_list':product_list})
 
 # Create your views here.
@@ -94,93 +94,90 @@ def update_data():
 	user_brand_mat.to_pickle('User_Brand_Label.pkl')
 
 
-def update_matrix():
-	dim=10
-	nu=20
-	n_p=40
-
-
-	user_mat=np.random.random((nu,dim))
-	prod_mat=np.random.random((n_p,dim))
-
-	labels_up=pd.read_pickle("User_Prod_Label.pkl")
-	labels_up=np.array(labels_up)
-
-	labels_uu=pd.read_pickle("User_User_Label.pkl")
-	labels_uu=np.array(labels_uu)
-
-	epochs=500
-	eta=0.005
-	lam=0.01
-
-	def sigmoid(arr):
-	    return 1/(1+np.exp(-1*arr))
-
-	def inv_sigmoid(arr):
-	    return sigmoid(arr)*(1-sigmoid(arr))
-
-	for i in range(epochs):
-	    z_up=user_mat.dot(prod_mat.T)
-	    y_up=sigmoid(z_up)
-	    grad_up=inv_sigmoid(z_up)
-	    error_up=y_up-labels_up
-
-	    user_mat = user_mat - (error_up*grad_up).dot(prod_mat)*eta
-	    prod_mat = prod_mat - ((error_up*grad_up).T).dot(user_mat)*eta
-
-
-	for i in range(epochs*2):
-	    z_uu=user_mat.dot(user_mat.T)
-	    y_uu=sigmoid(z_uu)
-	    grad_uu=inv_sigmoid(z_uu)
-	    error_uu=y_uu-labels_uu
-
-	    user_mat = user_mat - (error_uu*grad_uu).dot(user_mat)*eta
-	    user_mat = user_mat - ((error_uu*grad_uu).T).dot(user_mat)*eta
-
-	for i in range(epochs*2):
-
-	    z_ub=user_mat.dot(brand_mat.T)
-	    y_ub=sigmoid(z_ub)
-	    grad_ub=inv_sigmoid(z_ub)
-	    error_ub=y_ub-labels_ub
-
-	    user_mat = user_mat - (error_ub*grad_ub).dot(brand_mat)*eta
-	    brand_mat = brand_mat - ((error_ub*grad_ub).T).dot(user_mat)*eta
-
-	    if i%5==0:
-	        print("Updating Database....")
-
-
-def recommend_prod(pk):
-	relation=read_pickle('user_prod.pkl')
-	poss_prods=np.argsort(relation[pk+1])
-	return poss_prods
-
-def recommend_user(pk):
-	relation=read_pickle('user_user.pkl')
-	poss_friends=np.argsort(relation[pk+1])
-	return poss_friends
-
-def recommend_user(pk):
-	relation=read_pickle('user_brand.pkl')
-	poss_brands=np.argsort(relation[pk+1])
-	return poss_brands
-
-
-class ListHome(ListView):
-	update_data()
-	model=Customer
-	template_name="home/home.html"
-
-
-
-
-def shop(request):
-	return render(request,'home/shop.html')
-
-def sprod(request):
-	return render(request,'home/spdetails.html')
-
-
-
+# def update_matrix():
+# 	dim=20
+# 	nu=len(Customer.objects.all()
+# 	n_p=len(Product.objects.all()
+#
+#
+# 	user_mat=np.random.random((nu,dim))
+# 	prod_mat=np.random.random((n_p,dim))
+#
+# 	labels_up=pd.read_pickle("User_Prod_Label.pkl")
+# 	labels_up=np.array(labels_up)
+#
+# 	labels_uu=pd.read_pickle("User_User_Label.pkl")
+# 	labels_uu=np.array(labels_uu)
+#
+# 	epochs=500
+# 	eta=0.005
+# 	lam=0.01
+#
+# 	def sigmoid(arr):
+# 	    return 1/(1+np.exp(-1*arr))
+#
+# 	def inv_sigmoid(arr):
+# 	    return sigmoid(arr)*(1-sigmoid(arr))
+#
+# 	for i in range(epochs):
+# 	    z_up=user_mat.dot(prod_mat.T)
+# 	    y_up=sigmoid(z_up)
+# 	    grad_up=inv_sigmoid(z_up)
+# 	    error_up=y_up-labels_up
+#
+# 	    user_mat = user_mat - (error_up*grad_up).dot(prod_mat)*eta
+# 	    prod_mat = prod_mat - ((error_up*grad_up).T).dot(user_mat)*eta
+#
+#
+# 	for i in range(epochs*2):
+# 	    z_uu=user_mat.dot(user_mat.T)
+# 	    y_uu=sigmoid(z_uu)
+# 	    grad_uu=inv_sigmoid(z_uu)
+# 	    error_uu=y_uu-labels_uu
+#
+# 	    user_mat = user_mat - (error_uu*grad_uu).dot(user_mat)*eta
+# 	    user_mat = user_mat - ((error_uu*grad_uu).T).dot(user_mat)*eta
+#
+# 	for i in range(epochs*2):
+#
+# 	    z_ub=user_mat.dot(brand_mat.T)
+# 	    y_ub=sigmoid(z_ub)
+# 	    grad_ub=inv_sigmoid(z_ub)
+# 	    error_ub=y_ub-labels_ub
+#
+# 	    user_mat = user_mat - (error_ub*grad_ub).dot(brand_mat)*eta
+# 	    brand_mat = brand_mat - ((error_ub*grad_ub).T).dot(user_mat)*eta
+#
+# 	    if i%5==0:
+# 	        print("Updating Database....")
+#
+#
+# def recommend_prod(pk):
+# 	relation=read_pickle('user_prod.pkl')
+# 	poss_prods=np.argsort(relation[pk+1])
+# 	return poss_prods
+#
+# def recommend_user(pk):
+# 	relation=read_pickle('user_user.pkl')
+# 	poss_friends=np.argsort(relation[pk+1])
+# 	return poss_friends
+#
+# def recommend_user(pk):
+# 	relation=read_pickle('user_brand.pkl')
+# 	poss_brands=np.argsort(relation[pk+1])
+# 	return poss_brands
+#
+#
+# class ListHome(ListView):
+# 	update_data()
+# 	model=Customer
+# 	template_name="home/home.html"
+#
+#
+#
+#
+# def shop(request):
+# 	return render(request,'home/shop.html')
+#
+# def sprod(request):
+# 	return render(request,'home/spdetails.html')
